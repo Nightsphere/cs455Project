7542af4726a375f9cbc743b489b20f34f33e738c
Josh <nightsphere88@gmail.com>
2019-04-30 21:47:34 -0600

    Update Cleaner.java

--- src/main/java/cs455/spark/Cleaner.java
+++ src/main/java/cs455/spark/Cleaner.java
@@ -22,10 +22,13 @@ public class Cleaner {
         JavaRDD<String> colleges = db.toJavaRDD().map(row -> {
             String result = row.getString(0) + "," + row.getString(3) + "," + row.getString(5) + "," +
                     row.getString(378) + "," + row.getString(379) + "," + row.getString(1509) + "," +
-                    row.getString(1510)+","+row.getString(1394)+","+row.getString(61)+", "+row.getString(63) +", "+ row.getString(65)
-                    +","+row.getString(69)+","+row.getString(70)+","+row.getString(75)+","+row.getString(78)+","+row.getString(79)
-                    +","+row.getString(96)+","+row.getString(83)+","+row.getString(86)+","+row.getString(87)+","+row.getString(91)
-                    +","+row.getString(95)+","+row.getString(97)+","+row.getString(74)+","+row.getString(67);
+                    row.getString(1510) + "," + row.getString(1394) + "," + row.getString(61) + ", " +
+                    row.getString(63) + ", " + row.getString(65) + "," + row.getString(69) + "," +
+                    row.getString(70) + "," + row.getString(75) + "," + row.getString(78) + "," +
+                    row.getString(79) + "," + row.getString(96) + "," + row.getString(83) + "," +
+                    row.getString(86) + "," + row.getString(87) + "," + row.getString(91) + "," +
+                    row.getString(95) + "," + row.getString(97) + "," + row.getString(74) + "," +
+                    row.getString(67);
             return result;
         });
 
